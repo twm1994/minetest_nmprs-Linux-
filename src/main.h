9b30f01e3220dc20f1fc41734a74590398ee328e
Wade Tam (Lab) <tangweimeng@live.com>
2019-04-24 21:56:26 +0800

    Add/remove node not working, while map can be loaded

--- src/main.h
+++ src/main.h
@@ -1,24 +1,15 @@
-/*
-(c) 2010 Perttu Ahola <celeron55@gmail.com>
-*/
-
 #ifndef MAIN_HEADER
 #define MAIN_HEADER
-
 #include <jmutex.h>
-namespace jthread {} // JThread 1.2 support
-using namespace jthread; // JThread 1.3 support
-
+using namespace jthread;
 #define PI 3.14159
-
 #define FOV_ANGLE (PI/2.5)
-
+// =====For map=====
+#define MAP_BOTTOM 0
 // Change to struct settings or something
 extern s16 g_viewing_range_nodes;
 extern JMutex g_viewing_range_nodes_mutex;
-
 #include <fstream>
-
 // Debug streams
 extern std::ostream dout_con;
 extern std::ostream dout_client;
--- src/test.h
+++
@@ -1,7 +0,0 @@
-#ifndef TEST_HEADER
-#define TEST_HEADER
-
-void run_tests();
-
-#endif
-
